9beb51fcf5964556182eff043ecf8017caa6b7c5
eulucaslim <lucaslimacnt14@gmail.com>
2025-08-18 23:16:36 -0400

    Using If

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -11,9 +11,18 @@ func main() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
-	
+
 	var comando int
 	fmt.Scan(&comando)
-	fmt.Println("O endereço da variavel comando é", &comando)
 	fmt.Println("o comando escolhido foi", comando)
+
+	if comando == 1 {
+		fmt.Println("Monitorando ...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa")
+	} else {
+		fmt.Println("Não conheço este comando")
+	}
 }
