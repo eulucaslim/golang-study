eb817359995e83ef86f2ae32307b22ac2cb1dc7b
eulucaslim <lucaslimacnt14@gmail.com>
2025-08-18 23:50:42 -0400

    Using functions in Golang and Os pkg

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,20 +1,12 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-	nome := "Lucas"
-	versao := 1.1
-	fmt.Println("Olá sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
-
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
-
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("o comando escolhido foi", comando)
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -23,7 +15,27 @@ func main() {
 		fmt.Println("Exibindo logs...")
 	case 0:
 		fmt.Println("Saindo do programa")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+	nome := "Lucas"
+	versao := 1.1
+	fmt.Println("Olá sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+func exibeMenu(){
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("o comando escolhido foi", comandoLido)
+	return comandoLido
 }
